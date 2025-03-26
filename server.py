cbb800bae015319108c51b649e35fa647496ccc0
Iwo Staykov <259259@student.pwr.edu.pl>
2025-03-26 12:56:43 +0100

    start and main (Client class incomplete)

--- server.py
+++ server.py
@@ -26,3 +26,60 @@ class Client:
         except Exception as e:
             print(f"Nie udalo sie polaczyc z powodu: {e}")
             return False
+        
+    def start(self):
+        if not self.connect():
+            return
+
+        root = tk.Tk()
+        root.withdraw()
+        self.nickname = simpledialog.askstring("Nazwa uzytkownika", "Podaj nazwe uzytkownika", parent=root)
+
+        if not self.nickname:
+            print("Zamykam bo nie podales swojego imienia")
+            self.sock.close()
+            return
+
+        print(f"Witaj {self.nickname}")
+
+        # main thread of the app
+        gui_thread = threading.Thread(target=self.gui_loop)
+        gui_thread.daemon=True
+        gui_thread.start()
+
+        # thread for receiving messages
+        receive_thread = threading.Thread(target=self.receive)
+        receive_thread.daemon = True
+        receive_thread.start()
+
+        # we checked whether user could successfully connect
+        #self.send_message(f"Dolaczono do czatu", system=True)
+
+        return gui_thread, receive_thread
+    
+    def main():
+        # required arguments to run app: (local)host and port
+        #HOST = '127.0.0.1'
+        #PORT = 9090
+        parser = argparse.ArgumentParser(description="Klient czatu")
+        parser.add_argument('host', nargs='?', default='127.0.0.1', help='Adres serwera')
+        parser.add_argument('-p', '--port', type=int, default=9090, help='Port TCP (domyslnie 9090)')
+
+        args = parser.parse_args()
+
+        client = Client(args.host, args.port)
+        try:
+            # initializating connection with server
+            client.start()
+            while client.running:
+                pass
+            # gui and receiving threads constantly running
+        except KeyboardInterrupt:
+            print("\nPrzerwano przez uzytkownika")
+            client.stop()
+        except Exception as e:
+            print(f"Nieoczekiwany blad: {e}")
+            client.stop()
+
+    if __name__ == '__main__':
+        main()
